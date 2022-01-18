0d6e7d8029225b971ad4cbe6c85451491c67b8ca
Giovani Girardi <giovanigg08@gmail.com>
2022-01-18 20:13:20 -0300

    feat: Criado componente de Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,20 @@ import logoImg from "../../assets/logo.svg";
 
 import { Container, Content } from "./styles";
 
-export const Header = () => {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export const Header: React.FC<HeaderProps> = ({
+  onOpenNewTransactionModal,
+}) => {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dtmoney logo" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
